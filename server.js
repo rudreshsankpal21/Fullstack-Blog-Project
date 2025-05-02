a88f4232d7d2ffbf0b1c10100e5da59dfbbacc3b
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-02 20:27:22 +0530

    Basic server setup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("New Blog Project Starting");
+});
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
 });
