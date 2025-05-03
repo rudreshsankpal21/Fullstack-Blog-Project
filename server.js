fd9665d082966294a67ff59c48f7357df1f8cf8e
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-03 21:04:00 +0530

    Initialized MONGODB

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
   res.send("New Blog Project Starting");
 });
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+
+//Connect DB
+mongoose
+  .connect(process.env.MONGOOSE_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("DB connected Successfully");
+
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("DB connection failed");
+  });
