21cc93baab96a4af16408e6ed841428057203e43
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-04 15:05:29 +0530

    Logic for user registration

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -1,13 +1,28 @@
 const express = require("express");
-
+const User = require("../models/User");
 const registerRoute = express.Router();
 registerRoute
   .route("/auth/register")
   .get((req, res) => {
     res.render("register");
   })
-  .post((req, res) => {
-    res.send("POST register Route");
+  .post(async (req, res) => {
+    // Main logic for user Registration
+    const { username, email, password } = req.body;
+    try {
+      // Check if user exists
+      const user = await User.findOne({ email });
+      if (user) {
+        res.send("User already exists");
+      } else {
+        // Create new user
+        const newUser = new User({
+          username,
+          email,
+          password,
+        });
+      }
+    } catch (error) {}
   });
 
 module.exports = registerRoute;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const loginRoute = require("./routes/loginRoute");
 const registerRoute = require("./routes/registerRoute");
-
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware for passing form data
+app.use(express.urlencoded({ extended: true }));
 
 // EJS
 app.set("view engine", "ejs");
@@ -17,11 +19,11 @@ app.get("/", (req, res) => {
 
 //Login Route
 app.get("/auth/login", loginRoute);
-app.post("/login", loginRoute);
+app.post("/auth/login", loginRoute);
 
 //Register Route
 app.get("/auth/register", registerRoute);
-app.post("/register", registerRoute);
+app.post("/auth/register", registerRoute);
 
 //Connect DB
 mongoose
