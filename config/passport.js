0a3fba33e19ed807480d691d2758a3f97a0aeb74
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-08 14:30:28 +0530

    Configured passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
 module.exports = function (passport) {
   // Define the local strategy for email and password authentication
@@ -15,9 +16,34 @@ module.exports = function (passport) {
               message: "User not found with entered email",
             });
           }
+
           //   compare the provided password with the hashed password
-        } catch (error) {}
+          const isMatch = await bcrypt.compare(password, user.password);
+          if (!isMatch) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          //   Authentication successfull, return the object
+          return done(null, user);
+        } catch (error) {
+          return done(error);
+        }
       }
     )
   );
+
+  // serializedUser:Determines which data of the user object should be stored in the session. Here,we store the user ID
+  passport.serializedUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  //   Deserialize the user object based on the use ID stored in the session
+  try {
+    passport.deserializedUser(async function (id, done) {
+      const user = await User.findById(id);
+      done(null, user);
+    });
+  } catch (error) {
+    done(error);
+  }
 };
--- server.js
+++ server.js
@@ -2,11 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/authRoutes");
+const passport = require("passport");
+const passportConfig = require("./config/passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware for passing form data
 app.use(express.urlencoded({ extended: true }));
+
+// Session middleware
+app.use(
+  session({
+    secret: "itachi uchiha",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGOOSE_URL }),
+  })
+);
+
+// Passport configuration
+passportConfig(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // EJS
 app.set("view engine", "ejs");
