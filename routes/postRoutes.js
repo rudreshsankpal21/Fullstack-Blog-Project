c845b97c601df51ba0ba9c582f7385b51f860abe
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-09 21:59:03 +0530

    integrated multer and cloudinary

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { getPostForm, createPost } = require("../controllers/postController");
+const upload = require("../config/multer");
 const postRoutes = express.Router();
 
 // Get Post form route
 postRoutes.get("/add", getPostForm);
 
 //Creating new post route
-postRoutes.post("/add", createPost);
+postRoutes.post("/add", upload.array("image", 10), createPost);
 
 module.exports = postRoutes;
