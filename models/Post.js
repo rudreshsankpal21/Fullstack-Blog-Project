8781d8a914b19a623df278da9513a7aa1d4c7a56
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-09 21:32:40 +0530

    Updated PostSchema

--- models/Post.js
+++ models/Post.js
@@ -17,18 +17,18 @@ const postSchema = new mongoose.Schema(
       require: true,
       ref: "User",
     },
-    // images: [
-    //   {
-    //     url: {
-    //       type: String,
-    //        required: true,
-    //     },
-    //     public_id: {
-    //       type: String,
-    //       required: true,
-    //     },
-    //   },
-    // ],
+    images: [
+      {
+        url: {
+          type: String,
+          required: true,
+        },
+        public_id: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
     comments: [
       {
         type: mongoose.Schema.Types.ObjectId,
