5f2317c9a9a4ffe87947469f027098f0f1344054
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-10 22:53:58 +0530

    initialized errorHandler

--- controllers/postController.js
+++ controllers/postController.js
@@ -11,3 +11,53 @@ exports.getPostForm = asyncHandler((req, res) => {
     success: "",
   });
 });
+
+// Creating new post
+exports.createPost = asyncHandler(async (req, res) => {
+  const { title, content } = req.body;
+  // Validation
+  if (!req.files || req.files.length === 0) {
+    return res.render("newPost", {
+      title: "Create Post",
+      user: req.user,
+      error: "Atleast one Image is Mandatory",
+      success: "",
+    });
+  }
+
+  // Save the files into DB
+  const images = await Promise.all(
+    req.files.map(async (file) => {
+      // Save the images into the DB
+      const newFile = new Files({
+        url: file.path,
+        public_id: file.filename,
+        uploaded_by: req.user._id,
+      });
+      await newFile.save();
+      console.log(newFile);
+
+      return {
+        url: newFile.url,
+        public_id: newFile.public_id,
+      };
+    })
+  );
+
+  // Create the post
+  const newPost = new Post({
+    title,
+    content,
+    author: req.user._id,
+    images,
+  });
+
+  await newPost.save();
+
+  res.render("newPost", {
+    title: "Create Post",
+    user: req.user,
+    error: "",
+    success: "Post created successfully",
+  });
+});
