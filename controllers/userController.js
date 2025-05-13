5198ffc8db5d87cb9f2b8d4b2cb9e1b0ec4472b9
rudresh21 <rudreshsankpal21@gmail.com>
2025-05-13 10:07:57 +0530

    Initialized update profile logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/User");
 const Post = require("../models/Post");
-
+const cloudinary = require("../config/cloudinary");
+const Files = require("../models/File");
 // get user profile
 exports.getUserProfile = asyncHandler(async (req, res) => {
   // find the user
@@ -45,5 +46,49 @@ exports.editUserProfile = asyncHandler(async (req, res) => {
     user,
     error: "",
     success: "",
+  });
+});
+
+// update profile
+exports.updateUserProfile = asyncHandler(async (req, res) => {
+  const { username, email, bio } = req.body;
+  const user = await User.findById(req.user._id).select("-password");
+  if (!user) {
+    res.render("login", {
+      title: "login",
+      user: req.user,
+      error: "User not found",
+    });
+  }
+  user.username = username || user.username;
+  user.email = email || user.email;
+  user.bio = bio || user.bio;
+
+  // Delete old img from cloudinary
+  if (req.file) {
+    if (user.profilePicture && user.profilePicture.public_id) {
+      await cloudinary.uploader.destroy(user.profilePicture.public_id);
+    }
+  }
+
+  //create new file
+  const file = await Files({
+    url: req.file.path,
+    public_id: req.file.filename,
+    uploaded_by: req.user._id,
+  });
+
+  await file.save();
+  user.profilePicture = {
+    url: file.url,
+    public_id: file.public_id,
+  };
+
+  await user.save();
+  res.render("editProfile", {
+    title: "Edit Profile",
+    user,
+    error: "",
+    success: "Profile updated successfully",
   });
 });
